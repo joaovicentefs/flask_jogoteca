d657b439f2117eedd17f416ee45b92b8da8e01cc
Joao Vicente <joao@upsites.digital>
2021-05-18 00:03:04 -0300

    Continuando, agora podemos criar um jogo pela view,aprendendo a declarar o metodo POST e  como no html, atráves dos parâmetros da tag form declarar o post e o action(aponta para a rota)

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,17 +8,26 @@ class Jogo:
         self.categoria = categoria
         self.console = categoria
 
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Commandos', 'Ação', 'PC')
+lista = [jogo1, jogo2, jogo3]
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('Commandos', 'Ação', 'PC')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
 
-app.run()
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
