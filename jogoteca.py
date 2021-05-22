cd8921d2998f1116213fa86c74d9ee128c037144
Joao Vicente <joao@upsites.digital>
2021-05-22 18:27:26 -0300

    Aprendendo a criar uma nova rota para fazer o logout do usuário, ainda de maneira muito simplificada

--- jogoteca.py
+++ jogoteca.py
@@ -49,5 +49,11 @@ def autenticar():
         flash('Não logado, tente novamente!')
         return redirect('/login')
 
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect('/')
+
 
 app.run(debug=True)
