3e8865fd0768057a73ba0974b529cde0c11c69c1
Joao Vicente <joao@upsites.digital>
2021-05-24 22:53:26 -0300

    Aprendendo a criar autenticar multiplos usuários. Ainda usuando autenticação bem simples, sem proteção de senha e sem persitência dos dados

--- jogoteca.py
+++ jogoteca.py
@@ -2,11 +2,29 @@ from flask import Flask, render_template, request, redirect, session, flash, url
 
 app = Flask(__name__)
 app.secret_key = 'upsites'
+
+
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
+
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+
+usuario1 = Usuario('joao', 'João Vicente', '1234')
+usuario2 = Usuario('nico', 'Nico Steppat', '7a1')
+usuario3 = Usuario('flavio', 'Flávio', 'javascript')
+
+usuarios = {usuario1.id: usuario1,
+          usuario2.id: usuario2,
+          usuario3.id: usuario3}
+
 
 jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
 jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
@@ -44,11 +62,13 @@ def login():
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'mestra' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
         flash('Não logado, tente novamente!')
         return redirect(url_for('login'))
