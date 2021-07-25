532b6fba128287bdf5a42e3a30b37ae1f7add6f3
Joao Vicente <joao@upsites.digital>
2021-07-25 16:24:03 -0300

    Aprendizado principal: Como separar o código para deixar o arquivo do app mais limpo e deixando cada arquivo com sua responsabilidade. Importante saber que é possível inserir um import no meio do código e um caso prático de uso do if __name__ == '__main__':

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
-from models import Jogo, Usuario
-from dao import JogoDao, UsuarioDao
+from flask import Flask
 import mysql.connector
 import os
-import time
 
 
 app = Flask(__name__)
@@ -18,104 +15,7 @@ db = mysql.connector.connect(
 )
 app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 
-jogo_dao = JogoDao(db)
-usuario_dao = UsuarioDao(db)
+from views import *
 
-
-@app.route('/')
-def index():
-    lista = jogo_dao.listar()
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
-
-
-@app.route('/novo')
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
-
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    jogo = jogo_dao.salvar(jogo)
-    arquivo = request.files['arquivo']
-    timestamp = time.time()
-    upload_path = app.config['UPLOAD_PATH']
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-
-@app.route('/editar/<int:id>')
-def editar(id):
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('editar')))
-    jogo = jogo_dao.buscar_por_id(id)
-    nome_imagem = recupera_imagem(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=nome_imagem)
-
-
-@app.route('/atualizar', methods=['POST',])
-def atualizar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console, id=request.form['id'])
-    jogo_dao.salvar(jogo)
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    deleta_arquivo(jogo.id)
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-@app.route('/deletar/<int:id>')
-def deletar(id):
-    jogo = jogo_dao.buscar_por_id(id)
-    jogo_dao.deletar(id)
-    flash( jogo.nome + ' foi deletado')
-    return redirect(url_for('index'))
-
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima)
-
-
-@app.route('/autenticar', methods=['POST',])
-def autenticar():
-    usuario = usuario_dao.busca_por_id(request.form['usuario'])
-    if usuario:
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.id
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-    else:
-        flash('Não logado, tente novamente!')
-        return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-    flash('Nenhum usuário logado!')
-    return redirect(url_for('index'))
-
-@app.route('/uploads/<nome_arquivo>')
-def imagem(nome_arquivo):
-    return send_from_directory('uploads', nome_arquivo)
-
-def recupera_imagem(id):
-    for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
-        if f'capa{id}' in nome_arquivo:
-            return nome_arquivo
-
-def deleta_arquivo(id):
-    arquivo = recupera_imagem(id)
-    os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
